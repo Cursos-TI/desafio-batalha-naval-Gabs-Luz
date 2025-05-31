310a4d282e29765d7afaefec5f7d2236cbc0cdc7
Gabs-Luz <gabsocn@gmail.com>
2025-05-31 21:22:46 +0000

    Adição Nível Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,15 +5,65 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+        // 0 == Água
+    // 3 == Navio
+    char linha[10] = {'A', 'B','C','D','E','F','G','H','I','J'};
+    int Coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //1. Criando Tabuleiro
+    int Tabuleiro[10][10];
+
+    //preencher tudo com Água (0)
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            Tabuleiro[i][j] = 0;
+        }
+        
+    }
+    
+    //Colocar Navios (3)
+
+    //vertical
+    Tabuleiro[1][1] = 3; // linha 2, coluna B
+    Tabuleiro[2][1] = 3; // linha 3, coluna B
+    Tabuleiro[3][1] = 3; // linha 4, coluna B
+
+    //Horizontal
+    Tabuleiro[6][6] = 3; // linha 7, coluna G
+    Tabuleiro[6][7] = 3; // linha 7, coluna H
+    Tabuleiro[6][8] = 3; // linha 7, coluna I
+
+    //4. Exibindo o Tabuleiro
+    printf(" TABULEIRO BATALHA NAVAL \n");
+    printf("  ");
+    for ( int i = 0; i < 10; i++)
+    {
+        printf("%c ", linha[i]); //Imprime as letras 
+    }  
+    printf("\n");
+
+    //Números lateral
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", Coluna[i]); // imprime 1 2 3...
+        
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", Tabuleiro[i][j]); // mostra 0 (água) ou 3 (navio)
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
+
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
@@ -36,5 +86,4 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
 }
